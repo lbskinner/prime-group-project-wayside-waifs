9da8b397c382a229d374352537a4e067e447a2fe
Leila Skinner <beileiwang@gmail.com>
2020-06-02 17:22:09 -0500

    working on request form

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -24,7 +24,16 @@ import RequestForm from "../RequestForm/RequestForm";
 import EventPage from "../EventPage/EventPage";
 import EventDetailsPage from "../EventDetailsPage/EventDetailsPage";
 
+import { ThemeProvider, createMuiTheme } from "@material-ui/core/styles"; // for custom theme
 import "./App.css";
+
+const theme = createMuiTheme({
+  palette: {
+    // primary: "",
+    // secondary: "",
+    // error: "",
+  },
+});
 
 class App extends Component {
   componentDidMount() {
@@ -33,76 +42,78 @@ class App extends Component {
 
   render() {
     return (
-      <Router>
-        <div>
-          <Nav />
-          <Switch>
-            {/* Visiting localhost:3000 will redirect to localhost:3000/home */}
-            <Redirect exact from="/" to="/home" />
-            {/* Visiting localhost:3000/about will show the about page.
+      <ThemeProvider>
+        <Router>
+          <div>
+            <Nav />
+            <Switch>
+              {/* Visiting localhost:3000 will redirect to localhost:3000/home */}
+              <Redirect exact from="/" to="/home" />
+              {/* Visiting localhost:3000/about will show the about page.
             This is a route anyone can see, no login necessary */}
-            <Route exact path="/about" component={AboutPage} />
-            <Route exact path="/home" component={LandingPage} />
-            <Route
-              exact
-              path="/request"
-              authRedirect="/request"
-              component={RequestForm}
-            />
-            {/* For protected routes, the view could show one of several things on the same route.
+              <Route exact path="/about" component={AboutPage} />
+              <Route exact path="/home" component={LandingPage} />
+              <Route
+                exact
+                path="/request"
+                authRedirect="/request"
+                component={RequestForm}
+              />
+              {/* For protected routes, the view could show one of several things on the same route.
             Visiting localhost:3000/user will show the UserPage if the user is logged in.
             If the user is not logged in, the ProtectedRoute will show the 'Login' or 'Register' page.
             Even though it seems like they are different pages, the user is always on localhost:3000/user */}
-            <ProtectedRoute exact path="/admin" component={UserPage} />
-            {/* This works the same as the other protected route, except that if the user is logged in,
+              <ProtectedRoute exact path="/admin" component={UserPage} />
+              {/* This works the same as the other protected route, except that if the user is logged in,
             they will see the info page instead. */}
-            <ProtectedRoute exact path="/info" component={InfoPage} />
-            {/* This works the same as the other protected route, except that if the user is logged in,
+              <ProtectedRoute exact path="/info" component={InfoPage} />
+              {/* This works the same as the other protected route, except that if the user is logged in,
             they will be redirected to the authRedirect path provided. */}
-            <ProtectedRoute
-              exact
-              path="/login"
-              authRedirect="/admin"
-              component={LoginPage}
-            />
-            <ProtectedRoute
-              exact
-              path="/registration"
-              authRedirect="/admin"
-              component={RegisterPage}
-            />
+              <ProtectedRoute
+                exact
+                path="/login"
+                authRedirect="/admin"
+                component={LoginPage}
+              />
+              <ProtectedRoute
+                exact
+                path="/registration"
+                authRedirect="/admin"
+                component={RegisterPage}
+              />
 
-            <ProtectedRoute
-              exact
-              path="/reports"
-              authRedirect="/reports"
-              component={ReportPage}
-            />
-            {/* <ProtectedRoute
+              <ProtectedRoute
+                exact
+                path="/reports"
+                authRedirect="/reports"
+                component={ReportPage}
+              />
+              {/* <ProtectedRoute
               exact
               path="/request"
               authRedirect="/request"
               component={RequestForm}
             /> */}
-            <ProtectedRoute
-              exact
-              path="/event"
-              authRedirect="/event"
-              component={EventPage}
-            />
-            <ProtectedRoute
-              exact
-              path="/eventDetails"
-              authRedirect="/eventDetails"
-              component={EventDetailsPage}
-            />
+              <ProtectedRoute
+                exact
+                path="/event"
+                authRedirect="/event"
+                component={EventPage}
+              />
+              <ProtectedRoute
+                exact
+                path="/eventDetails"
+                authRedirect="/eventDetails"
+                component={EventDetailsPage}
+              />
 
-            {/* If none of the other routes matched, we will show a 404. */}
-            <Route render={() => <h1>404</h1>} />
-          </Switch>
-          <Footer />
-        </div>
-      </Router>
+              {/* If none of the other routes matched, we will show a 404. */}
+              <Route render={() => <h1>404</h1>} />
+            </Switch>
+            <Footer />
+          </div>
+        </Router>
+      </ThemeProvider>
     );
   }
 }
--- src/components/RequestForm/RequestForm.js
+++ src/components/RequestForm/RequestForm.js
@@ -1,11 +1,14 @@
 import React, { Component } from "react";
 import { connect } from "react-redux";
 import mapStoreToProps from "../../redux/mapStoreToProps";
+import TextField from "@material-ui/core/TextField";
+import Grid from "@material-ui/core/Grid";
+import Typography from "@material-ui/core/Typography";
+import Paper from "@material-ui/core/Paper";
+import Select from "@material-ui/core/Select";
+import MenuItem from "@material-ui/core/MenuItem";
+import FormControl from "@material-ui/core/FormControl";
 
-// Basic class component structure for React with default state
-// value setup. When making a new component be sure to replace
-// the component name TemplateClass with the name for the new
-// component.
 class RequestForm extends Component {
   state = {
     heading: "Class Component",
@@ -14,7 +17,124 @@ class RequestForm extends Component {
   render() {
     return (
       <div>
-        <h2>{this.state.heading}</h2>
+        <Paper>
+          <Typography variant="h4">Humane Education Request Form</Typography>
+          <Typography variant="h6">Contact Information</Typography>
+          <Grid container spacing={4}>
+            <Grid item>
+              <TextField label="First Name" variant="outlined" />
+            </Grid>
+            <Grid item>
+              <TextField label="Last Name" variant="outlined" />
+            </Grid>
+            <Grid item>
+              <TextField label="Email" variant="outlined" />
+            </Grid>
+            <Grid item>
+              <TextField label="Phone Number" variant="outlined" />
+            </Grid>
+          </Grid>
+        </Paper>
+        <br />
+        <Paper>
+          <Typography variant="h6">School or Organization Name</Typography>
+          <TextField variant="outlined" fullWidth />
+        </Paper>
+        <Paper>
+          <Grid container spacing={3}>
+            <Grid item>
+              <Typography variant="h6">Estimated Number of Students</Typography>
+              <TextField variant="outlined" />
+            </Grid>
+            <Grid item>
+              <Typography variant="h6">
+                Please indicate the grade level and age of students
+              </Typography>
+              <TextField variant="outlined" />
+            </Grid>
+            <Grid item>
+              <Typography variant="h6">Number of Adult Sponsors</Typography>
+              <TextField variant="outlined" />
+            </Grid>
+          </Grid>
+        </Paper>
+        <Paper>
+          <Typography variant="h6">
+            Which Program are you interested in?
+          </Typography>
+          <FormControl variant="outlined">
+            <Select
+              displayEmpty
+              //  value={age}
+              //  onChange={handleChange}
+            >
+              <MenuItem value="">Select a program</MenuItem>
+              <MenuItem value="FIA">
+                "Kindness in Action!" (formerly Families in Action)
+              </MenuItem>
+              <MenuItem value="NMB">"No More Bullying!®"</MenuItem>
+              <MenuItem value="DS">
+                PAW-etiquette for Pooches & People: Dog Safety
+              </MenuItem>
+              <MenuItem value="AE">
+                Activating Em-PAW-thy: Exploring Similarities between Pets and
+                People
+              </MenuItem>
+              <MenuItem value="OUT">
+                Once U-PAW-n a Time Reading Program
+              </MenuItem>
+              <MenuItem value="KIA">Kids-in-Action</MenuItem>
+              <MenuItem value="ET">Educational Tours</MenuItem>
+              <MenuItem value="">Other</MenuItem>
+            </Select>
+          </FormControl>
+          {/* the text input field will conditional render, if other is selected */}
+          <TextField variant="outlined" label="Enter program name" />
+        </Paper>
+        <Paper>
+          <Typography variant="h6">Preferred location of Program</Typography>
+          <FormControl variant="outlined">
+            <Select
+              displayEmpty
+              //  value={age}
+              //  onChange={handleChange}
+            >
+              <MenuItem value="">Select a location</MenuItem>
+              <MenuItem value="on_site">Wayside Waifs</MenuItem>
+              <MenuItem value="off_site">Other</MenuItem>
+            </Select>
+          </FormControl>
+          {/* the text input field will conditional render, if other is selected */}
+          <Typography>
+            * Please note that in order to be good stewards of our resources we
+            are unable to deliver programming at locations outside of a 30
+            minute radius of Wayside Waifs.
+          </Typography>
+          <TextField variant="outlined" label="Enter full address" fullWidth />
+        </Paper>
+        <Paper>
+          <Typography variant="h6">Preferred date of program</Typography>
+          <FormControl variant="outlined">
+            <Select
+              displayEmpty
+              //  value={age}
+              //  onChange={handleChange}
+            >
+              <MenuItem value="">Select a location</MenuItem>
+              <MenuItem value="on_site">Wayside Waifs</MenuItem>
+              <MenuItem value="off_site">Other</MenuItem>
+            </Select>
+          </FormControl>
+          {/* the text input field will conditional render, if other is selected */}
+          <Typography>
+            Because weekends are our busiest days for adoptions, educational
+            programs are typically held Monday-Friday.
+          </Typography>
+          <TextField
+            variant="outlined"
+            label="will be a calendar, placeholder for now"
+          />
+        </Paper>
       </div>
     );
   }
