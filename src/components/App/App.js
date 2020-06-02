feb4bc39b840be6862ea9047727f6e1a7e7bff7c
Leila Skinner <beileiwang@gmail.com>
2020-06-02 15:17:56 -0500

    update the link for request form as not protected route

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -43,6 +43,12 @@ class App extends Component {
             This is a route anyone can see, no login necessary */}
             <Route exact path="/about" component={AboutPage} />
             <Route exact path="/home" component={LandingPage} />
+            <Route
+              exact
+              path="/request"
+              authRedirect="/request"
+              component={RequestForm}
+            />
             {/* For protected routes, the view could show one of several things on the same route.
             Visiting localhost:3000/user will show the UserPage if the user is logged in.
             If the user is not logged in, the ProtectedRoute will show the 'Login' or 'Register' page.
@@ -72,12 +78,12 @@ class App extends Component {
               authRedirect="/reports"
               component={ReportPage}
             />
-            <ProtectedRoute
+            {/* <ProtectedRoute
               exact
               path="/request"
               authRedirect="/request"
               component={RequestForm}
-            />
+            /> */}
             <ProtectedRoute
               exact
               path="/event"
