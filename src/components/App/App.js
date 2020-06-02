6798b6f9790af5cc9f054ac901615080ae8d844f
Leila Skinner <beileiwang@gmail.com>
2020-06-02 14:23:14 -0500

    added link to app.js

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,30 +1,34 @@
-import React, {Component} from 'react';
+import React, { Component } from "react";
 import {
   HashRouter as Router,
   Route,
   Redirect,
   Switch,
-} from 'react-router-dom';
+} from "react-router-dom";
 
-import {connect} from 'react-redux';
+import { connect } from "react-redux";
 
-import Nav from '../Nav/Nav';
-import Footer from '../Footer/Footer';
+import Nav from "../Nav/Nav";
+import Footer from "../Footer/Footer";
 
-import ProtectedRoute from '../ProtectedRoute/ProtectedRoute'
+import ProtectedRoute from "../ProtectedRoute/ProtectedRoute";
 
-import AboutPage from '../AboutPage/AboutPage';
-import UserPage from '../UserPage/UserPage';
-import InfoPage from '../InfoPage/InfoPage';
-import LandingPage from '../LandingPage/LandingPage';
-import LoginPage from '../LoginPage/LoginPage';
-import RegisterPage from '../RegisterPage/RegisterPage';
+import AboutPage from "../AboutPage/AboutPage";
+import UserPage from "../UserPage/UserPage";
+import InfoPage from "../InfoPage/InfoPage";
+import LandingPage from "../LandingPage/LandingPage";
+import LoginPage from "../LoginPage/LoginPage";
+import RegisterPage from "../RegisterPage/RegisterPage";
+import ReportPage from "../ReportPage/ReportPage";
+import RequestForm from "../RequestForm/RequestForm";
+import EventPage from "../EventPage/EventPage";
+import EventDetailsPage from "../EventDetailsPage/EventDetailsPage";
 
-import './App.css';
+import "./App.css";
 
 class App extends Component {
-  componentDidMount () {
-    this.props.dispatch({type: 'FETCH_USER'})
+  componentDidMount() {
+    this.props.dispatch({ type: "FETCH_USER" });
   }
 
   render() {
@@ -37,32 +41,16 @@ class App extends Component {
             <Redirect exact from="/" to="/home" />
             {/* Visiting localhost:3000/about will show the about page.
             This is a route anyone can see, no login necessary */}
-            <Route
-              exact
-              path="/about"
-              component={AboutPage}
-            />
-            <Route
-              exact
-              path="/home"
-              component={LandingPage}
-            />
+            <Route exact path="/about" component={AboutPage} />
+            <Route exact path="/home" component={LandingPage} />
             {/* For protected routes, the view could show one of several things on the same route.
             Visiting localhost:3000/user will show the UserPage if the user is logged in.
             If the user is not logged in, the ProtectedRoute will show the 'Login' or 'Register' page.
             Even though it seems like they are different pages, the user is always on localhost:3000/user */}
-            <ProtectedRoute
-              exact
-              path="/admin"
-              component={UserPage}
-            />
+            <ProtectedRoute exact path="/admin" component={UserPage} />
             {/* This works the same as the other protected route, except that if the user is logged in,
             they will see the info page instead. */}
-            <ProtectedRoute
-              exact
-              path="/info"
-              component={InfoPage}
-            />
+            <ProtectedRoute exact path="/info" component={InfoPage} />
             {/* This works the same as the other protected route, except that if the user is logged in,
             they will be redirected to the authRedirect path provided. */}
             <ProtectedRoute
@@ -78,13 +66,39 @@ class App extends Component {
               component={RegisterPage}
             />
 
+            <ProtectedRoute
+              exact
+              path="/reports"
+              authRedirect="/reports"
+              component={ReportPage}
+            />
+            <ProtectedRoute
+              exact
+              path="/request"
+              authRedirect="/request"
+              component={RequestForm}
+            />
+            <ProtectedRoute
+              exact
+              path="/event"
+              authRedirect="/event"
+              component={EventPage}
+            />
+            <ProtectedRoute
+              exact
+              path="/eventDetails"
+              authRedirect="/eventDetails"
+              component={EventDetailsPage}
+            />
+
             {/* If none of the other routes matched, we will show a 404. */}
             <Route render={() => <h1>404</h1>} />
           </Switch>
           <Footer />
         </div>
       </Router>
-  )}
+    );
+  }
 }
 
 export default connect()(App);
