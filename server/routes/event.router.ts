efbd4669111ffa90cf67bd1884cd1ef453bba9b3
Matt Halvorson <halvorson34.mh@gmail.com>
2020-06-03 13:42:04 -0500

    created GET route for events in event.router.ts

--- server/routes/event.router.ts
+++ server/routes/event.router.ts
@@ -1,21 +1,37 @@
 import { Request, Response } from "express";
-import express from 'express';
-import pool from '../modules/pool';
+import express from "express";
+import pool from "../modules/pool";
 
 const router: express.Router = express.Router();
 
 /**
  * GET route template
  */
-router.get('/', (req: Request, res: Response, next: express.NextFunction): void => {
+router.get(
+  "/",
+  (req: Request, res: Response, next: express.NextFunction): void => {
+    const queryText = `SELECT * FROM "event";`;
 
-});
+    pool
+      .query(queryText)
+      .then((responseDb) => {
+        res.send(responseDb.rows);
+      })
+      .catch((err) => {
+        console.warn(err);
+        res.sendStatus(500);
+      });
+  }
+);
 
 /**
  * POST route template
  */
-router.post('/', (req: Request, res: Response, next: express.NextFunction): void => {
+router.post(
+  "/",
+  (req: Request, res: Response, next: express.NextFunction): void => {
     res.sendStatus(201);
-});
+  }
+);
 
-module.exports = router;
+export default router;
