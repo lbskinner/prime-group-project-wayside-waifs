1f0bf13fdbac8e2252b9c36fa838910ccbf10b20
Matt Halvorson <halvorson34.mh@gmail.com>
2020-06-03 16:10:16 -0500

    initial GET route for reports in report.router.ts

--- server/server.ts
+++ server/server.ts
@@ -4,6 +4,7 @@ import sessionMiddleware from "./modules/session-middleware";
 import passport from "./strategies/user.strategy";
 import userRouter from "./routes/user.router";
 import eventRouter from "./routes/event.router";
+import reportRouter from "./routes/report.router";
 
 require("dotenv").config();
 
@@ -23,6 +24,7 @@ app.use(passport.session());
 /* Routes */
 app.use("/api/user", userRouter);
 app.use("/api/event", eventRouter);
+app.use("/api/report", reportRouter);
 
 // Serve static files
 app.use(express.static("build"));
