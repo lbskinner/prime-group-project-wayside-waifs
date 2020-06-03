4feb585850084ae958362d0132c29f72d84e2278
Adam Bungart <abungart@gmail.com>
2020-06-03 15:18:52 -0500

    Individual Event List Data prepared

--- src/components/EventPageComponent/MyEvent/MyEvent.js
+++ src/components/EventPageComponent/MyEvent/MyEvent.js
@@ -10,7 +10,15 @@ class MyEvent extends Component {
     return (
       <div>
         {this.props.event.educator_user_id === this.props.store.user.id && (
-          <div></div>
+          <div onClick={this.eventDetails}>
+            <p>
+              {this.props.event.organization}
+              <span>{this.props.event.request_date}</span>
+            </p>
+            <p>Program Date: {this.props.event.program_date}</p>
+            <p>Program Requested: {this.props.event.training_program}</p>
+            <button onClick={this.assign}>Assign</button>
+          </div>
         )}
       </div>
     );
--- src/components/EventPageComponent/RequestedEvent/RequestedEvent.js
+++ src/components/EventPageComponent/RequestedEvent/RequestedEvent.js
@@ -4,12 +4,21 @@ import mapStoreToProps from "../../../redux/mapStoreToProps";
 
 class RequestedEvent extends Component {
   render() {
-    return ( <div>{this.props.event.status === "Requested" && (
-        <div>
-            
-        </div>)
-        });
-        </div>
+    return (
+      <div>
+        {this.props.event.status === "Requested" && (
+          <div onClick={this.eventDetails}>
+            <p>
+              {this.props.event.organization}
+              <span>{this.props.event.request_date}</span>
+            </p>
+            <p>Program Date: {this.props.event.program_date}</p>
+            <p>Program Requested: {this.props.event.training_program}</p>
+            <button onClick={this.assign}>Assign</button>
+          </div>
+        )}
+      </div>
+    );
   }
 }
 export default connect(mapStoreToProps)(RequestedEvent);
