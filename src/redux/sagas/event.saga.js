bb66b9f316fb0f2879a0e1b5eaa3523e125e075c
Leila Skinner <beileiwang@gmail.com>
2020-06-05 13:44:18 -0500

    added sucessful submittion request alert and error alert for request form

--- src/redux/sagas/event.saga.js
+++ src/redux/sagas/event.saga.js
@@ -1,11 +1,19 @@
 import axios from "axios";
 import { put, takeEvery, takeLatest } from "redux-saga/effects";
+import swal from "sweetalert";
 
 // save new event request to database
 function* saveRequest(action) {
   try {
     // don't need the config since it does not require login to save events
-    yield axios.post("/api/request/new", action.payload);
+    const response = yield axios.post("/api/request/new", action.payload);
+    if (response.data === "Created") {
+      swal(
+        "Thank you for submitting your request. An educator will contact you soon."
+      );
+    } else {
+      swal("Oops, something went wrong, please try again!");
+    }
   } catch (error) {
     console.log("Save new event request failed", error);
   }
