aaaffd5e3e5b2020e5ae00919f8ea14ab890c1d6
Leila Skinner <beileiwang@gmail.com>
2020-06-07 16:31:48 -0500

    added login modal to navbar login link, still need to figure out how to push to the event page

---
+++ src/components/LoginPage/LoginPageModal.js
@@ -0,0 +1,93 @@
+import React, { Component } from "react";
+import { connect } from "react-redux";
+import mapStoreToProps from "../../redux/mapStoreToProps";
+import { withRouter } from "react-router-dom";
+
+class LoginPageModal extends Component {
+  state = {
+    username: "",
+    password: "",
+  };
+
+  login = (event) => {
+    event.preventDefault();
+
+    if (this.state.username && this.state.password) {
+      this.props.dispatch({
+        type: "LOGIN",
+        payload: {
+          username: this.state.username,
+          password: this.state.password,
+        },
+      });
+      this.props.handleCloseModal();
+      this.props.history.push("/event");
+    } else {
+      this.props.dispatch({ type: "LOGIN_INPUT_ERROR" });
+    }
+  }; // end login
+
+  handleInputChangeFor = (propertyName) => (event) => {
+    this.setState({
+      [propertyName]: event.target.value,
+    });
+  };
+
+  render() {
+    return (
+      <div>
+        <form onSubmit={this.login}>
+          <h1>Login</h1>
+          {this.props.store.errors.loginMessage && (
+            <p className="alert" role="alert">
+              {this.props.store.errors.loginMessage}
+            </p>
+          )}
+          <div>
+            <label htmlFor="username">
+              Username:
+              <input
+                type="text"
+                name="username"
+                value={this.state.username}
+                onChange={this.handleInputChangeFor("username")}
+              />
+            </label>
+          </div>
+          <div>
+            <label htmlFor="password">
+              Password:
+              <input
+                type="password"
+                name="password"
+                value={this.state.password}
+                onChange={this.handleInputChangeFor("password")}
+              />
+            </label>
+          </div>
+          <div>
+            <input
+              // className="log-in"
+              type="submit"
+              name="submit"
+              value="Log In"
+            />
+          </div>
+        </form>
+        <center>
+          <button
+            type="button"
+            // className="link-button"
+            onClick={() => {
+              this.props.dispatch({ type: "SET_TO_REGISTER_MODE" });
+            }}
+          >
+            Register
+          </button>
+        </center>
+      </div>
+    );
+  }
+}
+
+export default withRouter(connect(mapStoreToProps)(LoginPageModal));
--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -2,11 +2,23 @@ import React from "react";
 import { Link } from "react-router-dom";
 import { connect } from "react-redux";
 import LogOutButton from "../LogOutButton/LogOutButton";
+import Modal from "@material-ui/core/Modal";
 import "./Nav.css";
 import mapStoreToProps from "../../redux/mapStoreToProps";
 import logoImage from "../../images/nav_logo3.png";
+import LoginPageModal from "../LoginPage/LoginPageModal";
 
 const Nav = (props) => {
+  const [open, setOpen] = React.useState(false);
+
+  const handleOpen = () => {
+    setOpen(true);
+  };
+
+  const handleClose = () => {
+    setOpen(false);
+  };
+
   let loginLinkData = {
     path: "/login",
     text: "Login",
@@ -14,7 +26,7 @@ const Nav = (props) => {
 
   if (props.store.user.id != null) {
     loginLinkData.path = "/event";
-    loginLinkData.text = "Home";
+    loginLinkData.text = "Event";
   }
 
   return (
@@ -24,7 +36,7 @@ const Nav = (props) => {
           target="_black"
           href="https://secure.waysidewaifs.org/site/SPageServer/?pagename=Home_new"
         >
-          <img src={logoImage} alt className="nav-logo" />
+          <img src={logoImage} alt="" className="nav-logo" />
         </a>
         {/* <Link to="/home">
         <h2 className="nav-title">Wayside Waifs Humane Education </h2>
@@ -34,6 +46,9 @@ const Nav = (props) => {
           <Link className="nav-link" to="/request">
             Request Event
           </Link>
+          <button className="nav-link" onClick={handleOpen}>
+            Login Modal
+          </button>
           <Link className="nav-link" to={loginLinkData.path}>
             {/* Show this link if they are logged in or not,
           but call this link 'Home' if they are logged in,
@@ -63,6 +78,11 @@ const Nav = (props) => {
           <img src={logoImage} alt className="nav-logo" />
         </a>
       </div> */}
+      <Modal open={open} onClose={handleClose}>
+        <div className="modalContent">
+          <LoginPageModal handleCloseModal={handleClose} />
+        </div>
+      </Modal>
     </div>
   );
 };
